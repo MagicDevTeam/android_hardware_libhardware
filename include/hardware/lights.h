d6ebed6614ac42501114735d6d5c28917e498b98
SunRain <41245110@qq.com>
2014-01-30 01:13:48 +0800

    Revert "lights: Add music light support (1/2)"
    This reverts commit f42ef9077386488eb213f0a4f093767b4f0a2b1a.
    
    Change-Id: I2f06d0301a5d17a928568d04a591bc5cca2da187

--- include/hardware/lights.h
+++ include/hardware/lights.h
@@ -52,13 +52,12 @@ __BEGIN_DECLS
 #define LIGHT_ID_WIFI               "wifi"
 
 /*
- * Additional hardware-specific lights
- */
-#define LIGHT_ID_CAPS               "caps"
-#define LIGHT_ID_FUNC               "func"
-#define LIGHT_ID_WIMAX              "wimax"
-#define LIGHT_ID_FLASHLIGHT         "flashlight"
-#define LIGHT_ID_MUSIC              "music"
+* Additional hardware-specific lights
+*/
+#define LIGHT_ID_CAPS "caps"
+#define LIGHT_ID_FUNC "func"
+#define LIGHT_ID_WIMAX "wimax"
+#define LIGHT_ID_FLASHLIGHT "flashlight"
 
 /* ************************************************************************
  * Flash modes for the flashMode field of light_state_t.
